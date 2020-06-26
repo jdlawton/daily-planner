2033d7e241924436d2ff5473a98b2702f793f289
JoeL <jlawton1203@gmail.com>
2020-06-26 16:00:09 -0500

    events are saved to localStorage and loaded when the application opens

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,27 @@ $("#currentDay").text(today);
 
 //loads events from localStorage
 var loadEvents = function() {
+    events = JSON.parse(localStorage.getItem("events"));
+    console.log("Inside loadEvents");
+    console.log(events);
+    if (!events){
+        events = [];
+    }
+    for (var i = 0; i < events.length; i++){
+        console.log ("looping through the loaded events: " + i);
+        console.log(events[i].time);
+        console.log(events[i].text);
+        var loadingBlock = $(".hour:contains('"+ events[i].time +"')");
+        console.log(loadingBlock);
+        loadingBlock.siblings(".text-box").children(".event-text").text(events[i].text);
 
+    }
 };
 
 //creates a new event
-var createEvent = function(){
-
+var saveEvents = function(){
+    console.log("inside saveEvents");
+    localStorage.setItem("events", JSON.stringify(events));
 };
 
 
@@ -28,18 +43,29 @@ $(".time-block").on("click", ".text-box", function () {
     textInput.trigger("focus");
 });
 
+$("textarea").on("blur", function() {
+    console.log("Inside event listener for textarea blur");
+});
+
 $(".save-btn").on("click", function() {
-    //console.log("Clicked on a button");
-    //console.log(this);
-    //get <textarea>'s current text/value
+    //get the value of the text in the <textarea> you are saving
     var text = $(this).siblings(".text-box").children("textarea").val();
     console.log(text);
 
+    //create a <p> element, save the <textarea> value to it and then replace
+    //the <textarea>
     var textInput = $("<p>").addClass("event-text").text(text);
     var replaceTxt = $(this).siblings(".text-box").children("textarea");
     replaceTxt.replaceWith(textInput);
     
-
-    
+    //get the text indicating which time block was clicked on, i.e. 7am
+    var blockLabel = $(this).siblings(".hour").text();
+    //console.log(blockLabel);
+    console.log("time: " + blockLabel);
+    console.log("text: " + text);
+    events.push({time: blockLabel, text: text});
+    console.log(events);
+    saveEvents();
 });
 
+loadEvents();
